81ffd7c0f1efa8257c30acd013d3cda94dcf56bf
borj4 <river4car@gmail.com>
2022-03-20 21:20:42 +0100

    update: throw user registered exception

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,26 +3,35 @@ import {
     Get, Post, Put, Patch,
     Req, Res,
     HttpStatus,
-    Body
+    NotFoundException,
+    Body, Param
 } from '@nestjs/common';
+import { NotificationsService } from 'src/notifications/notifications.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
-    constructor( private usersService: UsersService) {}
+    constructor(
+        private usersService: UsersService,
+        private notificationService: NotificationsService
+    ) {}
 
     @Post('/create')
-    async createUser(@Res() res, @Body() createUserDto: CreateUserDto){
+    async createUser(
+        @Res() res,
+        @Body() createUserDto: CreateUserDto
+    ){
+        const find = await this.usersService.findByEmail(createUserDto.email);
 
-        // console.log(createUserDto);
-
-        const user = await this.usersService.createUser(createUserDto)
-        return res.status(HttpStatus.OK).json({
-            message: 'received',
-            user: user,
-        })
+        if (find) { throw new NotFoundException('User is already registered'); }
+        else {
+            const user = await this.usersService.createUser(createUserDto)
+            return res.status(HttpStatus.OK).json({
+                message: 'Succesfully registered',
+                user: user,
+            });
+        }
     }
-
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,19 @@
-import { Injectable, Post } from '@nestjs/common';
-
-import { Model } from "mongoose";
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from "mongoose";
 import { User } from 'src/schemas/users.schema';
-
 import { CreateUserDto } from './dto/create-user.dto';
-
 
 @Injectable()
 export class UsersService {
     constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
     public async createUser( createUserDto: CreateUserDto ): Promise<User> {
-        
         const user = new this.userModel(createUserDto);
         return await user.save()
+    };
 
+    public async findByEmail(email: string): Promise<User> {
+        return await this.userModel.findOne({ email }, '-__v -messages').exec();
     }
 }
